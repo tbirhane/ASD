57aa4f0ac857f64d1ae89e90af150c5afc6c9502
Tsige Birhane <tsige.birhane@walmartlabs.com>
2021-05-09 17:35:30 -0700

    lab 12 dependency injection and spring

---
+++ SpringBootProject_12/src/main/java/product/Application.java
@@ -0,0 +1,22 @@
+package product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private ProductService productService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		productService.addProduct();
+	}
+}
